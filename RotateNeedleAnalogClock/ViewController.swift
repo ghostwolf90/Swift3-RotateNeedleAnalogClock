e01337c72f6f6792845f09796caa577075b274c3
laibit <ghostwolf90@gmail.com>
2017-07-06 18:40:13 +0800

    阿媛神幫助

--- RotateNeedleAnalogClock/ViewController.swift
+++ RotateNeedleAnalogClock/ViewController.swift
@@ -2,16 +2,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var labTime: UILabel!
+    
     // 画像インスタンス
     let ClockBackImage = UIImageView()
     let ClockTimehourImage = UIImageView()
     let pinImage = UIImageView()
     let MinuteTimehourImage = UIImageView()
     
+    // 目前時間
+    var curHour: Int = 0
+    var curMinutes: Int = 0
+    var oldMinutes: Int = 0
     
     var HourView: UIView!
     var MinuteView: UIView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -62,16 +68,22 @@ class ViewController: UIViewController {
         MinuteView.addSubview(MinuteBox)
         update()
     }
-
+    
     
     //　ドラッグ時に呼ばれる
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
- 
+        
         let touch = touches.first!
         if touch.view === HourView.subviews[0] {
             let position = touch.location(in: self.view)
             let target = HourView.center
-            let angle = atan2f(Float(target.y-position.y), Float(target.x-position.x)) - Float(M_PI) / Float(2)
+            let angle = atan2f(Float(target.y-position.y), Float(target.x-position.x)) - Float(Double.pi) / Float(2)
+            
+            let h: Float = (angle / 6.28319) * 12.0
+            if h < 0 { curHour = Int( h - 1 ) + 12 }
+            else { curHour = Int( h ) }
+            print("angle=\(angle) hour=\(curHour) minute=\(curMinutes)")
+            
             HourView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
             ClockTimehourImage.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
         }
@@ -79,9 +91,37 @@ class ViewController: UIViewController {
         if touch.view === MinuteView.subviews[0] {
             let position = touch.location(in: self.view)
             let target = MinuteView.center
-            let angle = atan2f(Float(target.y-position.y), Float(target.x-position.x)) - Float(M_PI) / Float(2)
+            let x: Float = Float(target.x-position.x)
+            let y: Float = Float(target.y-position.y)
+            
+            let angle = atan2f(y, x) - Float(Double.pi) / Float(2)
+            // angle range: 1.5708 ~ -4.71239
+            
+            let m: Float = (angle / 6.28319) * 60.0
+            if m < 0 { curMinutes = Int( m - 1 ) + 60 }
+            else { curMinutes = Int( m ) }
+            print("hour=\(curHour) minute=\(curMinutes) oldMinutes=\(oldMinutes) m=\(m)")
+            
+            if oldMinutes > 50 && curMinutes < 10 {
+                curHour += 1
+                oldMinutes = curMinutes
+            } else if oldMinutes < 10 && curMinutes > 50 {
+                curHour -= 1
+                oldMinutes = curMinutes
+            } else {
+                oldMinutes = curMinutes
+            }
+            labTime.text = "目前時間 \(curHour):\(curMinutes)"
             MinuteView.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
             MinuteTimehourImage.transform = CGAffineTransform(rotationAngle: CGFloat(angle))
+            
+            var mm = curHour * 60 + curMinutes
+            if mm > 195 { mm -= 720 }
+            let hourAngle = Float(mm) / 720.0 * 6.28319
+            print("hourAngle=\(hourAngle)")
+            
+            HourView.transform = CGAffineTransform(rotationAngle: CGFloat(hourAngle))
+            ClockTimehourImage.transform = CGAffineTransform(rotationAngle: CGFloat(hourAngle))
         }
     }
     
@@ -92,6 +132,9 @@ class ViewController: UIViewController {
         let hour = calendar.component(.hour, from: date)
         let minutes = calendar.component(.minute, from: date)
         let seconds = calendar.component(.second, from: date)
+        curHour = hour
+        curMinutes = minutes
+        oldMinutes = minutes
         let hDeg = (hour % 12) * (360 / 12);
         let mDeg = minutes * (360 / 60);
         let sDeg = seconds * (360 / 60)
@@ -108,11 +151,11 @@ class ViewController: UIViewController {
         ClockTimehourImage.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi * Double(hDeg) / 180.0))
     }
     
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
